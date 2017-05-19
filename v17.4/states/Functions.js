d9c754b9841cedf12a4110635bd1d31005b88af8
CharlesYnov <charlescoumet@gmail.com>
2017-05-19 19:27:01 +0200

    changement du sol

---
+++ v17.4/states/Functions.js
@@ -0,0 +1,169 @@
+function initVariables()
+{
+    mort = false;
+    routeV = -6;
+    objectSpeed = -300;
+    interframeSpeed = 0;
+    score = 0;
+    compteur = 0;
+    limiteInterframe = Math.round(Math.random()) * 70;
+    interframe = 0;
+    vitesseJoueur = 10;
+    vitesseShuriken = 10;
+    scoreToChangeBackground1 = 530;
+    scoreToChangeBackground2 = 1670;
+}
+
+
+function updateSpeed()
+{
+    if (interframeSpeed > 20)
+    {
+        routeV -= 0.05;
+        objectSpeed = routeV * 61.5;
+        vitesseJoueur = -routeV * 1.7;
+        vitesseShuriken = -routeV;
+        interframeSpeed = 0;
+    }
+    interframeSpeed ++;
+}
+
+
+function updateScore()
+{
+    if (compteur == 10)
+    {
+        score += 2;
+        compteur = 0;
+        scoreText.text = 'Score: ' + score;
+    }
+    compteur ++;
+}
+
+
+function launchRandomObject()
+{
+    if (interframe > limiteInterframe && score < 2750)
+    {
+        random = Math.round(Math.random() * 300);
+        if ((random % 3 == 0) && shuriken.x < 300 && pique.x < 300 && enemy.x < 300)
+        {
+            launchPique();
+        }
+        else if ((random % 3 == 1) && shuriken.x < 300 && pique.x < 300 && enemy.x < 300)
+        {
+            if (score > scoreToChangeBackground1)
+            {
+                launchShuriken();
+            }
+        }
+        else if ((random % 3 == 2) && shuriken.x < 300 && pique.x < 300 && enemy.x < 300)
+        {
+            if (score > scoreToChangeBackground2)
+            {
+                launchEnemy();
+            }
+        }
+        limiteInterframe = Math.round(Math.random()) * 70;
+        interframe = 0;
+    }
+    interframe ++;
+
+}
+
+
+function launchShuriken()
+{
+    shuriken = game.add.sprite(900, 380,'shuriken');
+    game.physics.arcade.enable(shuriken, Phaser.Physics.ARCADE);
+    animshuriken = shuriken.animations.add('animshuriken', [0,1,2,3]);
+    shuriken.animations.play('animshuriken', vitesseShuriken, true);
+    shuriken.body.allowGravity = false;
+    shuriken.body.velocity.x = objectSpeed;
+    shuriken.body.setSize(32, 32, 3, 5);
+}
+
+
+function launchPique()
+{
+    pique = game.add.sprite(900, 445,'pique');
+    game.physics.arcade.enable(pique, Phaser.Physics.ARCADE);
+    pique.body.allowGravity = false;
+    pique.body.velocity.x = objectSpeed;
+    pique.body.setSize(3, 64, 24);
+}
+
+
+function launchEnemy()
+{
+    enemy = game.add.sprite(900, 390, 'enemy');
+    game.physics.arcade.enable(enemy, Phaser.Physics.ARCADE);
+    enemy.body.allowGravity = false;
+    enemy.body.velocity.x = objectSpeed;
+}
+
+
+function eventEnemy()
+{
+    if (this.cursors.right.isDown && game.physics.arcade.overlap(player, enemy))
+    {
+        enemy.alpha = 0;
+    }
+    else
+    {
+        gameOver();
+    }
+}
+
+
+function changeBackground1At(scoreToChangeBackground)
+{
+    if (score == scoreToChangeBackground - 70)
+    {
+        routeV = -7;
+    }
+    else if (score == scoreToChangeBackground)
+    {
+        bg1.visible = false;
+        route.visible = false;
+    }
+}
+
+
+function changeBackground2At(scoreToChangeBackground)
+{
+    if (score == scoreToChangeBackground - 88)
+    {
+        routeV = -8;
+    }
+    else if (score == scoreToChangeBackground)
+    {
+        bg2.visible = false;
+        pont.visible = false;
+    }
+}
+
+
+function gameOver()
+{
+    player.animations.play('animmort', 10, false);
+    bgSound.pause();
+    mortSound.play();
+    game.time.events.add(Phaser.Timer.SECOND * 1, startGameOver, this);
+    mort = true;
+    pique.body.velocity.x = 0;
+    shuriken.body.velocity.x = 0;
+}
+
+function win(){
+    if(score > 2810){
+        game.state.start('Win');
+    }
+}
+
+
+function startGameOver()
+{
+    game.state.start('GameOver');
+}
+
---
+++ v17.4/states/Win.js
@@ -0,0 +1,30 @@
+var winState = {
+
+    create: function ()
+    {
+        var colorText = '#000000';
+        game.stage.backgroundColor = "#000000";
+        bgWin = game.add.sprite(0, 0, 'bgWin');
+
+
+        game.add.text(20, 20, 'Félicitations ! Vous avez terminé NinjaRunner !', {
+            font: '35px njnaruto',
+            fill: colorText
+        });
+        meilleurScore = score;
+
+    },
+
+    update: function ()
+    {
+        // Appuyer entrer pour rejouer
+        var key = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        key.onDown.addOnce(this.restart, this);
+    },
+
+    restart: function ()
+    {
+        game.state.start('Game');
+    }
+
+};
